87cb1ebbd65cd0bd84178c56b442e5e296504541
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 12:43:15 -0500

    Add member variable name for Bank class

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Bank {
+    private String name;
     private ArrayList<Branch> branches = new ArrayList<Branch>();
     public void addNewBranch()
     {
