96448ecd6823af25594df5ef0e19bd675bca59fe
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 09:00:14 -0500

    Create addNewBranch, addCustomer, addTransaction, showCustomers methods to Bank class

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,18 @@
 public class Bank {
+    private void addNewBranch()
+    {
+
+    }
+    private void addCustomer()
+    {
+
+    }
+    private void addTransaction()
+    {
+
+    }
+    private void showCustomers()
+    {
+
+    }
 }
