323e52f40629b532be9d91d8b2a4a7e23e4fdb5b
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 09:22:12 -0500

    Make constructor, getters and setters for Branch class

--- src/Branch.java
+++ src/Branch.java
@@ -1,6 +1,28 @@
 public class Branch {
     private String branchId;
     private String branchName;
+
+    public Branch(String branchId, String branchName) {
+        this.branchId = branchId;
+        this.branchName = branchName;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
     private void addNewCustomer(String customerName)
     {
 
