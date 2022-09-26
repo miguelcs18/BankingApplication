ac8c6d8c5a0b6209dc19ec415444b48ba49f4729
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 12:36:25 -0500

    Remove branchId member variable as branchId will be generated by de index of branches

--- src/Branch.java
+++ src/Branch.java
@@ -1,19 +1,11 @@
 public class Branch {
-    private String branchId;
+
     private String branchName;
 
-    public Branch(String branchId, String branchName) {
-        this.branchId = branchId;
+    public Branch(String branchName) {
         this.branchName = branchName;
     }
 
-    public String getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(String branchId) {
-        this.branchId = branchId;
-    }
 
     public String getBranchName() {
         return branchName;
