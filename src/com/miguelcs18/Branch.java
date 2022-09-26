a008170bad29faf56c49168655b37e0cc880be04
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 13:57:33 -0500

    Add findCustomer method and logic

--- src/com/miguelcs18/Branch.java
+++ src/com/miguelcs18/Branch.java
@@ -40,4 +40,17 @@ public class Branch {
         }
         return false;
     }
+
+    private Customer findCustomer(String customerName)
+    {
+        for(int i=0; i<this.customers.size(); i++)
+        {
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(customerName))
+            {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
 }
