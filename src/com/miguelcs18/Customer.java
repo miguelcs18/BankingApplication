bd33a7115ffd4bbede335240a5e60de1dfbe9167
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 13:19:57 -0500

    Replace the Double explicit with <>

--- src/com/miguelcs18/Customer.java
+++ src/com/miguelcs18/Customer.java
@@ -8,7 +8,7 @@ public class Customer {
 
     public Customer(String name, double initialAmount) {
         this.name = name;
-        this.transactions = new ArrayList<Double>();
+        this.transactions = new ArrayList<>();
         addTransaction(initialAmount);
     }
 
