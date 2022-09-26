6bd813d03c9177cef08ba7a85de1276f684eebb1
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 12:52:26 -0500

    Add constructor, getters and setters to customer class

--- src/Customer.java
+++ src/Customer.java
@@ -4,4 +4,34 @@ public class Customer {
     private String name;
     private String email;
     private ArrayList<Double> transactions = new ArrayList<>();
+
+    public Customer(String name, String email, ArrayList<Double> transactions) {
+        this.name = name;
+        this.email = email;
+        this.transactions = transactions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Double> transactions) {
+        this.transactions = transactions;
+    }
 }
