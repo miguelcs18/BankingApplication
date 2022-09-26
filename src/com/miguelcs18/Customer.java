70d7b71d260b813152c54c3e8a2fc591066b7b4f
miguelcs18 <miguelcs18@gmail.com>
2022-09-26 13:12:33 -0500

    Remove email member variable

--- src/com/miguelcs18/Customer.java
+++ src/com/miguelcs18/Customer.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 
 public class Customer {
     private String name;
-    private String email;
     private ArrayList<Double> transactions;
 
     public Customer(String name, String email, ArrayList<Double> transactions) {
         this.name = name;
-        this.email = email;
         this.transactions = transactions;
     }
 
@@ -19,14 +17,6 @@ public class Customer {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public ArrayList<Double> getTransactions() {
