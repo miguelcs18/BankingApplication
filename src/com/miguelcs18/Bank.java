5201cd532b4a2d22e1dd64b1ec372737adde4fae
miguelcs18 <miguelcs18@gmail.com>
2022-09-27 08:14:04 -0500

    Add Branch constructor and initialize branches ArrayList

--- src/com/miguelcs18/Bank.java
+++ src/com/miguelcs18/Bank.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Bank {
     private String name;
     private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>();
+    }
+
     public void addNewBranch()
     {
     }
